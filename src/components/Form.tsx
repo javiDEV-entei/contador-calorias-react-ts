338cbba81045fa387d62d06ece6aa27cfc16b770
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-04-27 11:09:08 -0300

    estilos main container add
    se agregaron los estilos para el contenedor principal en categoria actividad y calorias

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,55 @@
-
+import { categories } from "../data/categories"
 
 export default function Form() {
   return (
-    <div>Form</div>
+    <form className=" space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className=" grid grid-cols-1 gap-3">
+            <label htmlFor="category" className="font-bold">Catogoria:</label>
+
+                <select className=" border-slate-300 p-2 rounded-lg w-full bg-white"
+                id="category">
+
+                  {categories.map(category =>(
+                    <option key={category.id} value={category.id}>
+
+                      {category.name}
+                    </option>
+                  ))}
+
+                </select>
+
+        </div>
+        <div className=" grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">Actividad:</label>
+
+        <input 
+        id="activity"
+        type="text"
+        className=" border-slate-300 p-2 rounded-lg"
+        placeholder="Ej. Comida, jugo de naranaja, ensalada, Ejercicio, Pesas, Bicicleta"
+         />
+                  
+        </div>
+
+        <div className=" grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calorias:</label>
+
+        <input 
+        id="calories"
+        type="number"
+        className=" border-slate-300 p-2 rounded-lg"
+        placeholder="Calorias: ej. 300 o 500"
+         />       
+        </div>
+
+        <input 
+        type="submit"
+        className="bg-gray-800 hover: bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value={"Guardar comida o Guardar ejercicio"}
+
+        />
+        
+
+    </form>
   )
 }
---
+++ src/data/categories.ts
@@ -0,0 +1,8 @@
+import { Category } from '../types/index';
+
+
+
+export const categories: Category[] = [
+    { id: 1, name: 'Comida'},
+    { id: 2, name: 'Ejercicio'}
+  ]
